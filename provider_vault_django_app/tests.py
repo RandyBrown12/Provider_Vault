5b1c14aed89ece53e42b2996e5cf4415689e8b3a
Randy <randybrown9812@gmail.com>
2025-09-04 08:59:19 -0500

    Add small test case for GitHub Actions

--- provider_vault_django_app/tests.py
+++ provider_vault_django_app/tests.py
@@ -1,1 +1,11 @@
-# Create your tests here.
+from django.test import TestCase, Client
+
+
+class HomePageTest(TestCase):
+    def test_home_page_status_code(self):
+        client = Client()
+        response = client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            b"Home", response.content
+        )  # Adjust 'Home' to match actual content
